4066d42dac57ef7787a247a7f1dda031f975c2c4
Taylor McKinney <2374670+taylormck@users.noreply.github.com>
2024-10-02 03:42:34 +0900

    test: add unit tests for tokenizer

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ fn main() {
 
     while input_chars.peek().is_some() {
         let token = token::read_token(&mut input_chars, &mut current_line, &mut current_column);
+
         match token {
             Ok(token) => {
                 dbg!(token);
--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,6 @@
 use std::{iter::Peekable, str::Chars};
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum TokenType {
     Int(i32),
     LeftParen,
@@ -12,10 +12,11 @@ pub enum TokenType {
     Star,
     Slash,
     Dot,
+    Bang,
     Eof,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct Token {
     pub token_type: TokenType,
     pub line: usize,
@@ -48,7 +49,7 @@ pub fn read_token(
             ' ' => None,
             '\n' => {
                 *current_line += 1;
-                *current_column = 1;
+                *current_column = 0;
                 None
             }
             '(' => Some(Token::new(
@@ -76,11 +77,13 @@ pub fn read_token(
             '*' => Some(Token::new(TokenType::Star, *current_line, *current_column)),
             '/' => Some(Token::new(TokenType::Slash, *current_line, *current_column)),
             '.' => Some(Token::new(TokenType::Dot, *current_line, *current_column)),
+            '!' => Some(Token::new(TokenType::Bang, *current_line, *current_column)),
             first_digit if first_digit.is_ascii_digit() => {
-                *current_column += 1;
+                let starting_column = *current_column;
+
                 let mut n = vec![first_digit];
 
-                while input.peek().is_some() {
+                while input.peek().is_some() && input.peek().unwrap().is_ascii_digit() {
                     n.push(input.next().unwrap());
                     *current_column += 1;
                 }
@@ -88,7 +91,11 @@ pub fn read_token(
                 let n = n.into_iter().collect::<String>();
 
                 match n.parse() {
-                    Ok(n) => Some(Token::new(TokenType::Int(n), *current_line, *current_line)),
+                    Ok(n) => Some(Token::new(
+                        TokenType::Int(n),
+                        *current_line,
+                        starting_column,
+                    )),
                     Err(_) => return Err(TokenError::InvalidNumberToken(n)),
                 }
             }
@@ -103,4 +110,68 @@ pub fn read_token(
     }
 
     Ok(Token::new(TokenType::Eof, *current_line, *current_column))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_single_character_tokens() {
+        let input = "( ) { }\n+ - * /\n. ! -420";
+        let mut input = input.chars().peekable();
+
+        let expected_tokens: Vec<Token> = vec![
+            Token::new(TokenType::LeftParen, 1, 1),
+            Token::new(TokenType::RightParen, 1, 3),
+            Token::new(TokenType::LeftBrace, 1, 5),
+            Token::new(TokenType::RightBrace, 1, 7),
+            Token::new(TokenType::Plus, 2, 1),
+            Token::new(TokenType::Minus, 2, 3),
+            Token::new(TokenType::Star, 2, 5),
+            Token::new(TokenType::Slash, 2, 7),
+            Token::new(TokenType::Dot, 3, 1),
+            Token::new(TokenType::Bang, 3, 3),
+            Token::new(TokenType::Minus, 3, 5),
+            Token::new(TokenType::Int(420), 3, 6),
+        ];
+
+        let mut actual_tokens: Vec<Token> = vec![];
+
+        let mut current_line = 1;
+        let mut current_column = 1;
+
+        while input.peek().is_some() {
+            let token = read_token(&mut input, &mut current_line, &mut current_column).unwrap();
+            actual_tokens.push(token);
+        }
+
+        assert_eq!(expected_tokens, actual_tokens);
+    }
+
+    #[test]
+    fn test_integers() {
+        let input = "1 10 1234 -420";
+        let mut input = input.chars().peekable();
+
+        let expected_tokens: Vec<Token> = vec![
+            Token::new(TokenType::Int(1), 1, 1),
+            Token::new(TokenType::Int(10), 1, 3),
+            Token::new(TokenType::Int(1234), 1, 6),
+            Token::new(TokenType::Minus, 1, 11),
+            Token::new(TokenType::Int(420), 1, 12),
+        ];
+
+        let mut actual_tokens: Vec<Token> = vec![];
+
+        let mut current_line = 1;
+        let mut current_column = 1;
+
+        while input.peek().is_some() {
+            let token = read_token(&mut input, &mut current_line, &mut current_column).unwrap();
+            actual_tokens.push(token);
+        }
+
+        assert_eq!(expected_tokens, actual_tokens);
+    }
 }
